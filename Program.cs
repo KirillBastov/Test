cf3499674c6c7e4fe7f835da098eddb6e8c24362
KirillBastov <shvetskirill1994@gmail.com>
2021-07-21 20:38:33 +0300

    no message

--- Program.cs
+++ Program.cs
@@ -6,81 +6,44 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            bool isa = true;
-            bool ira = false; //bool = true или false 
-            Console.WriteLine(isa);
-            Console.WriteLine(ira);
+            // 1.
+            byte q = 255; // sbyte
+            Console.WriteLine(q - 20);
+            short w = 15000; //ushort
+            Console.WriteLine(w / 100);
+            int f = 10000232; //uint
+            Console.WriteLine(f * 2);
+            long x = 2323232323232323233; //ulong
+            Console.WriteLine(x + 7);
+            float g = 2.323f; 
+            Console.WriteLine(g + 2.32);
+            double j = 2.23323323;
+            Console.WriteLine(j + g);
+            var d = 32;
+            Console.WriteLine(d + 60 - 32);
 
-            byte by1 = 1;
-            byte by2 = 100; ///byte = 0-255
-
-            Console.WriteLine(by1);
-            Console.WriteLine(by2);
-
-
-            sbyte sb1 = -5;
-            sbyte sb2 =127; // sbyte -127 до +12
-
-            Console.WriteLine(sb1);
-            Console.WriteLine(sb2);
-
-
-
-            short sh1 = 15000; 
-            Console.WriteLine(sh1);
-
-            ushort sh2 = 15000;
-            Console.WriteLine(sh2);
+            //  2.
+            int x1 = 7;
+            int x2 = x1 * 3;
+            int x3 = x1 + x2;
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
+            Console.WriteLine(x3);
 
 
-            int a =-1000023232;
-            Console.WriteLine(a);
-
-            uint b = 2323232323;
-            Console.WriteLine(b);
-
-
-            long x = -2323232323232323233;
-            Console.WriteLine(x);
-
-            ulong f = 2332323232323232323;
-            Console.WriteLine(f);
-
-
-            float g = 2.323f; // числа с плавающей точкой 
-            double j = 2.23323323; // число с плавающей точкой уневерсальное как int 
-
-            char sim = 'a';  // юникод символы 
-            String gagag = "ямадама "; // текст 
-            Console.WriteLine(gagag);
-
-            object obj = 22;
-
-            // стек + куча -  ссылочные 
-
-            var d = "вывыв"; // уневерсальный тип 
-
-
+            //  3.
+            Console.WriteLine("Введите первое число ");
+            double a1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число ");
+            double a2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите третье число ");
+            double a3 = Convert.ToDouble(Console.ReadLine());
+            double j3 = a1 / a2;
+            double j4 = a1 + a2 + a3;
+            Console.WriteLine($"Деление {j3}{'\n'}Сложение {j4}");
             
-
-            dasdsadsa
-                sadasdsad
-                dasdasd
-
-
-
-
-
-
-
-
-
-
-
-
 
 
         }
-
     }
 }
